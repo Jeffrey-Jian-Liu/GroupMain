3ca69388e921191eed404a904529a2f2410f7b56
Jeffrey Liu <jeffrey.jian.liu@hotmail.com>
2018-03-28 21:07:32 +1100

    run as lib

--- app/src/main/java/com/liubase/groupmain/MainActivity.kt
+++ app/src/main/java/com/liubase/groupmain/MainActivity.kt
@@ -2,11 +2,19 @@ package com.liubase.groupmain
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    
+    private val myTag = "Main Activity"
+    
+    private val pAL : ArrayList<FrameLayout> = ArrayList()
+    private var pIndex : Int = 2
+    private val fm = fragmentManager
     
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
     }
 }
